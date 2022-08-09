10e5893c212335e00f57b57f9b4c0c2ce84bead8
Abdullateef Ayodele Salaudeen <Salaudeen Abdullateef>
2022-08-09 07:28:29 +0100

    initial commit

---
+++ lib/Constants.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color (0xff1d1e33);
+const kInactiveCardColor = Color(0XFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+    fontSize: 18.0,
+    color: Color(0XFF8D8E98)
+);
+
+const kNumberTextStyle = TextStyle(
+fontSize: 50.0,
+fontWeight: FontWeight.w900,
+);
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const kResultTextStyle = TextStyle(
+  color: Color(0XFF24D876),
+  fontSize: 22.0,
+      fontWeight:FontWeight.bold,
+);
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
