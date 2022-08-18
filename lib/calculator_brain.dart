01ad479703088294c70b01f03f532f6478524803
Abdullateef Ayodele Salaudeen <Salaudeen Abdullateef>
2022-08-18 09:18:43 +0100

    initial commit

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -3,15 +3,7 @@ class CalculatorBrain {
 
   CalculatorBrain({this.height, this.weight});
 
-  final int height;
-  final int weight;
 
-  double _bmi;
-
-  String calculateBMI(){
-     _bmi = weight /pow(height/100, 2);
-   return _bmi.toStringAsFixed(2);
-  }
   String getResult(){
     if(_bmi >= 25){
       return 'Overweight';
